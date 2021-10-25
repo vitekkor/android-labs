856623507431f90462846703944c30d87af8eb61
vitekkor <viktor.korotkikh@gmail.com>
2021-10-25 22:50:18 +0300

    Minor fixes

--- Lab2/myapplication/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ Lab2/myapplication/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -2,10 +2,13 @@ package ru.spbstu.icc.kspt.lab2.continuewatch
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    @Volatile
     var secondsElapsed: Int = 0
     lateinit var textSecondsElapsed: TextView
 
@@ -13,35 +16,47 @@ class MainActivity : AppCompatActivity() {
         const val SECONDS = "SECONDS"
     }
 
-    var backgroundThread = Thread {
-        while (true) {
-            Thread.sleep(1000)
-            textSecondsElapsed.post {
-                textSecondsElapsed.text = "Seconds elapsed: " + secondsElapsed++
+    private fun getNewBackgroundThread() = Thread {
+        try {
+            while (true) {
+                Thread.sleep(1000)
+                Log.d("BKGND", "Running")
+                textSecondsElapsed.post {
+                    textSecondsElapsed.text = getString(R.string.secondsElapsed, secondsElapsed++)
+                }
             }
+        } catch (e: InterruptedException) {
+            Log.d("BKGND", "Interrupted")
         }
     }
+
+    private lateinit var backgroundThread: Thread
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
-        backgroundThread.start()
-    }
-
-    override fun onResume() {
         val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
         secondsElapsed = sharedPref.getInt(SECONDS, 0)
-        super.onResume()
+        textSecondsElapsed.text = getString(R.string.secondsElapsed, secondsElapsed)
     }
 
-    override fun onPause() {
+    override fun onStart() {
         val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
-        with (sharedPref.edit()) {
+        secondsElapsed = sharedPref.getInt(SECONDS, 0)
+        backgroundThread = getNewBackgroundThread()
+        backgroundThread.start()
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
             putInt(SECONDS, secondsElapsed)
             apply()
         }
-        super.onPause()
+        backgroundThread.interrupt()
     }
 
     /*override fun onSaveInstanceState(outState: Bundle) {
