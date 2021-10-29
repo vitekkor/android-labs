28c85c2435beda4ed34a1f104803b1981adda28a
vitekkor <viktor.korotkikh@gmail.com>
2021-10-29 19:29:40 +0300

    Lab3. Task 1.b

---
+++ Lab3/app/src/main/java/com/vitekkor/lab3/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.vitekkor.lab3
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vitekkor.lab3.ui.theme.Lab3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Lab3Theme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Conversation(SampleData.conversationSample)
+                }
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.hackerman),
+            contentDescription = "HackerMan",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    fontFamily = FontFamily.Monospace
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "Light", showBackground = true)
+@Preview(name = "Dark", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun Preview() {
+    Lab3Theme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
---
+++ Lab3/app/src/main/java/com/vitekkor/lab3/SampleData.kt
@@ -0,0 +1,83 @@
+package com.vitekkor.lab3
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "nagibator228",
+            "Попробуй нажать сюда"
+        ),
+        Message(
+            "xxX_JOKE_MASTER_Xxx",
+            "Жена отправляет мужа-программиста в магазин:\n" +
+                    "— Купи батон хлеба, если будут яйца — возьми десяток.\n" +
+                    "Муж возвращается из магазина с десятью батонами.\n" +
+                    "— Ты зачем столько хлеба купил?\n" +
+                    "— Так ведь яйца были..."
+        ),
+        Message(
+            "Adam Arabov",
+            """Тут ничего нет, можешь не нажимать.
+ ___________________
+ |                 | 
+ | пока ничего нет |
+ |_________________|
+       ||
+ (\(\  ||
+ ( -.-)づ
+ O_(")(")""".trimIndent()
+        ),
+        Message(
+            "Adam Arabov",
+            "А я говорил..."
+        ),
+        Message(
+            "Я устал придумывать имена",
+            "Текст тоже не знаю какой придумать, поэтому тут будет просто какая-то ерунда"
+        ),
+        Message(
+            "Colleague",
+            "Kotlin лучше Java. Change my mind"
+        ),
+        Message(
+            "Elon Musk",
+            "Am thinking of starting new university:\n" +
+                    "Texas Institute of Technology & Science"
+        ),
+        Message(
+            "Vitekkor",
+            "А я, кстати, в JetBrains Research работаю"
+        ),
+        Message(
+            "Hackerman",
+            "Я знаю, где ты живёшь"
+        ),
+        Message(
+            "Hackerman",
+            """       
+                         
+                   .-''''''_.
+                 .' _      _ '.
+                /   0      0   \
+               :                :
+               |                |
+               :       __       :
+                \  .-"`  `"-.  /
+                 '.          .'
+                   '-......-'
+            YOU SHOULDN'T BE HERE""".trimIndent()
+        ),
+        Message(
+            "Colleague",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Colleague",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Colleague",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
