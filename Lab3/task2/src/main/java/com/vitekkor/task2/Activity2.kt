6b409a1c08c37646273f11ebf25572f3ba424232
vitekkor <viktor.korotkikh@gmail.com>
2021-11-02 16:49:38 +0300

    Lab3. Task 2

---
+++ Lab3/task2/src/main/java/com/vitekkor/task2/Activity2.kt
@@ -0,0 +1,50 @@
+package com.vitekkor.task2
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.vitekkor.task2.databinding.Activity2Binding
+
+
+class Activity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+            val binding = Activity2Binding.inflate(layoutInflater)
+            binding.toFirst.setOnClickListener {
+                finish()
+            }
+            binding.toThird.setOnClickListener {
+                val intent = Intent(this, Activity3::class.java)
+                startActivityForResult(intent, 0)
+            }
+            setContentView(binding.root)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == 1) {
+            finish()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.about -> {
+                val intent = Intent(this, ActivityAbout::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
