7d4ac7ccb497e616bb1abc8d0727633b322d995e
vitekkor <viktor.korotkikh@gmail.com>
2021-11-03 14:42:39 +0300

    Lab3. Fixes

--- Lab3/task5/src/main/java/com/vitekkor/task5/MainActivity.kt
+++ Lab3/task5/src/main/java/com/vitekkor/task5/MainActivity.kt
@@ -3,10 +3,6 @@ package com.vitekkor.task5
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.vitekkor.task5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
