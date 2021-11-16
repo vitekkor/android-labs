8f7c5377db9d9729089a70e21235216191e506be
vitekkor <viktor.korotkikh@gmail.com>
2021-11-16 20:32:20 +0300

    Misc

--- Lab2/myapplication/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ Lab2/myapplication/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     @Volatile
